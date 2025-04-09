dc029a0c33a3a7f6286d3569f8ab22af070f6436
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-04-10 00:07:28 +0100

    Add function to draw a 5-pointed star using turtle graphics

---
+++ attempts/main.py
@@ -0,0 +1,19 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.title("Turtle Drawing Board")
+drawing_board.bgcolor("light blue")
+turtle_instance = turtle.Turtle()
+
+def star(side_length=100):
+    for i in range(5):
+        turtle_instance.left(216)
+        turtle_instance.forward(side_length)
+        
+        
+
+star()
+    
+
+turtle.done()
+
